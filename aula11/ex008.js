1c01b563cde0578fbfb7e21e61e85ca1dc76c4c5
ricsalomon56-wq <ricsalomon56@gmail.com>
2026-08-12 14:41:22 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
