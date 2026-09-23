ff3954ca74f23ecf61e9d0adad0256dd8d128fb7
ricsalomon56-wq <ricsalomon56@gmail.com>
2026-09-23 10:29:46 -0300

    gdfs

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -2,9 +2,10 @@ const numbers = []
 const tab = document.getElementById('sel')
 
 function adicionar() {
-    const input = Number(document.getElementById('num').value)
-
-    if (input === '' || Number.isNaN(num) || input < 1 || input > 100) {
+    const input = document.getElementById('num')
+    const num = Number(input.value)
+    
+    if (input.value === '' || Number.isNaN(num) || input < 1 || input > 100) {
         windows.alert('Digite um numero valido!')
         return
     }
@@ -14,14 +15,41 @@ function adicionar() {
     tab.innerHTML = ''
 
     numbers.forEach((numero, indice) => {
+        
         const item = document.createElement('option')
 
-        item.textContent = numero
+        item.textContent = `O valor adicionado foi ${numero}`
         item.value = `tab${indice}`
 
         tab.appendChild(item)
 
     })
     
-   input.valueOf = ''
+    input.value = ''
+}
+
+
+function finalizar() {
+
+    const crescente = [...numbers].sort((a,b) => a-b)
+    const tam = numbers.length
+    const maior = crescente[tam - 1]
+    const menor = crescente[0]
+    
+    let soma = 0
+    for (let c = 0; c < tam; c++) {
+        soma += numbers[c]
+    }
+
+    const media = soma/tam
+    
+    const res = document.getElementById('finalizar')
+    
+    res.innerHTML = `
+        <p> Ao todo temos ${tam} números cadastrados. </p>
+        <p> O maior valor informado foi ${maior}. </p>
+        <p> O menor valor informado foi ${menor}. </p>
+        <p> Somando todos os valores, temos ${soma}. </p>
+        <p> A media dos valores ${media}. </p>
+    `
 }
